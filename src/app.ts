92689e34f16e13ac947ae6e87508cdd1a07a70b5
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-04-20 12:34:43 +0600

    refactor: clean up response messages and organize middleware usage

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,20 @@ import express, { Request, Response } from 'express'
 const app = express();
 const port = 3000
 
+//parser
+app.use(express.json());
+app.use(express.text());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World test4!')
+  res.send('Hello World!')
 })
 
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
+  // res.send("Hello World test4 post!");
+  res.json({
+    message: "Successfully Received Data!",
+  })
 });
 
 export default app;
