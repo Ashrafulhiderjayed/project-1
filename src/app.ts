e9d32705820bfe6e39ac83397a54fb0611dc0ce8
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-04-20 12:15:24 +0600

    refactor: add TypeScript types to request and response in app routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 // const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World test4!')
 })
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+});
 
 export default app;
