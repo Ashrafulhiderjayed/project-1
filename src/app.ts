67aafbcef765e5d60145113f5bbd151ccdecd06c
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-04-21 13:37:15 +0600

    feat: add logger middleware to log request details

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,14 @@ const port = 3000
 app.use(express.json());
 app.use(express.text());
 
+const logger = (req: Request, res: Response, next: Function) => {
+  console.log(req.url, req.method, req.hostname); 
+  //req.url = /
+  //req.method = GET, POST, PUT, DELETE, PATCH
+  //req.hostname = localhost:5000
+  next();
+};
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
