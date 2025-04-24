e08be4bf0186407e0a3652770a734153f60964a0
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2025-04-24 17:16:38 +0600

    feat: enhance error handling and logging middleware in app routes handle not found route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
 // const express = require('express')
 const app = express();
@@ -36,6 +36,21 @@ courseRouter.post('/create-course', (req: Request, res: Response) =>{
   })
 })
 
+// error handling middleware
+app.get("/", async (req: Request, res: Response) => {
+  try{
+    // throw new Error("Error Occurred!")
+    res.send("Hello World test4!");
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error!",
+    })
+  }
+})
+
 
 const logger = (req: Request, res: Response, next: Function) => {
   console.log(req.url, req.method, req.hostname); 
@@ -45,16 +60,49 @@ const logger = (req: Request, res: Response, next: Function) => {
   next();
 };
 
+app.get(
+  "/",
+  logger,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send('something');
+    } catch (error) {
+      next(error);
+      // res.status(400).json({
+      //   success: false,
+      //   message: "failed to get data",
+      // });
+    }
+  }
+);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-app.post("/", (req: Request, res: Response) => {
+app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   // res.send("Hello World test4 post!");
   res.json({
     message: "Successfully Received Data!",
   })
 });
+
+// if the route is not found, send a 404 error
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({ 
+    success: false,
+    message: "Route Not Found!",
+  })
+})
+
+//Global Error Handler
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  console.log(err);
+  res.status(400).json({
+    success: false,
+    message: "Internal Server Error!",
+  })
+})
 
 export default app;
